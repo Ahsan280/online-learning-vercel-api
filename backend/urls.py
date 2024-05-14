06ad30f778e0685eb888b345264b3f644300b1c6
Ahsan280 <ahsanrehman281@gmail.com>
2024-05-14 18:25:56 +0500

    serve

--- backend/urls.py
+++ backend/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('userauth.urls')),
     path('api/main/', include('main.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT})
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
